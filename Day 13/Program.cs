e8f051ca2d9a3b68a5d4abbe0f5cc3e840d325fe
Leon McGowan-Crockett <leon.mcgowancrocket@fosterfreeman.com>
2020-12-14 16:28:21 +0000

    Day 13 Puzzle 2 works

--- Day 13/Program.cs
+++ Day 13/Program.cs
@@ -17,14 +17,46 @@ namespace Day_13
 
         #region Private Methods
 
+        private static (long, long) FindNext(long start, long jump, long a, long offsetA)
+        {
+            var valid = new List<long>();
+
+            long i = start;
+            while (true)
+            {
+                if (valid.Count == 2)
+                    return (valid[0], valid[1] - valid[0]);
+
+                i += jump;
+
+                if ((i + offsetA) % a == 0)
+                {
+                    valid.Add(i);
+                }
+            }
+        }
+
+        private static long IterativeSearch(List<(long, long)> busses)
+        {
+            long start = 0;
+            long step = 1;
+
+            for (int i = 0; i < busses.Count; i++)
+            {
+                var next = busses[i];
+
+                (start, step) = FindNext(start, step, next.Item1, next.Item2);
+            }
+
+            return start;
+        }
+
         private static void Main(string[] args)
         {
             var input = File.ReadAllLines(@"D:\AdventOfCode\AdventOfCode\Day 13\day13.txt");
 
             earliestDeparture = int.Parse(input[0]);
-
             busses = input[1].Split(',').Where(a => a != "x").Select(a => int.Parse(a)).ToList();
-
             busses2 = input[1].Split(',').ToList();
 
             Console.WriteLine(Puzzle1().ToString());
@@ -39,6 +71,26 @@ namespace Day_13
         }
 
         private static long Puzzle2()
+        {
+            var puzzle2Input = new List<(long, long)>();
+
+            var offset = 0;
+
+            foreach (var bus in busses2)
+            {
+                if (bus != "x")
+                    puzzle2Input.Add((long.Parse(bus), offset));
+
+                offset++;
+            }
+
+            return IterativeSearch(puzzle2Input);
+        }
+
+        /// <summary>
+        /// takes many years to run
+        /// </summary>
+        private static long Puzzle2BruteForce()
         {
             long n = 100000000000000;
 
